df314501755ca2653ce43bda069e00ad636a3c0f
Datnq <dat@beedu.vn>
2021-09-08 16:40:11 +0700

    fix port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const Zing = require("./until/ZingMp3");
 app.use(cors());
 app.get("/home", (req, res) => {
